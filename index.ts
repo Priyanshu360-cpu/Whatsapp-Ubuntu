97d06131568ab42184646ef8ff866361b7278df6
Priyanshu360-cpu <tpriyanshu775@gmail.com>
2022-04-05 10:24:33 +0530

    Added Notification Option

--- index.ts
+++ index.ts
@@ -1,9 +1,18 @@
-const { app, globalShortcut, BrowserWindow } = require("electron");
+const { app, globalShortcut, BrowserWindow, Notification} = require("electron");
 const url = require("url");
 const path = require('path')
+const electron = require('electron');
+const ignored=["whatsapp","WhatsApp Web","WhatsApp"];
 const { clipboard } = require('electron')
 const {getCurrentWindow} = require('electron');
-
+const options = {
+  title: 'Whatsapp',
+  subtitle: 'LINUX',
+  body: 'You Recieved a new Notification ',
+  silent: false,
+  icon: path.join(__dirname, './images/174879.png'),
+  hasReply: true
+}
 const USER_AGENT = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36';
 function newApp() {
     const win = new BrowserWindow({
@@ -16,6 +25,18 @@ function newApp() {
         if(win.webContents.zoomLevel<=3)
         win.webContents.zoomLevel+=1;
     });
+    win.on('page-title-updated', () => {
+      const title = win.getTitle();
+      var o=0;
+      for(let i=0;i<ignored.length;i++){
+if(title==ignored[i]) o=1;
+      }
+      if(o==0){
+        let myNotification = new Notification(options);
+        myNotification.show();
+      };
+      o=0;
+    })
     globalShortcut.register("CommandOrControl+O", () => {
       if(win.webContents.zoomLevel>=0)
       win.webContents.zoomLevel-=1;
